134fdc22e4a50b69e7e3f690293e951a71e88792
yanakhorolska <yana.khorolska@protonmail.com>
2022-09-23 20:54:33 +0200

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,5 +1,5 @@
-﻿export function fetchCountries(searchName) {
-    return fetch(`https://restcountries.com/v3.1/name/${searchName}?fields=name,capital,population,flags,languages`)
+﻿export function fetchCountries(name) {
+    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
         .then(response => {
             if (response.ok) {
                 return response.json();
